194112f57fe4136a52490578b14bd3f1df79fb2c
carlos rodrigues dias <dias2001.rodrigues@gmail.com>
2023-11-18 19:49:36 +0100

    feature<ajoutmedic>: commencement du design

--- APPGSB/AjoutMedic.Designer.cs
+++ APPGSB/AjoutMedic.Designer.cs
@@ -28,12 +28,70 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lbFamille = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbFamille
+            // 
+            this.lbFamille.BackColor = System.Drawing.Color.PowderBlue;
+            this.lbFamille.FormattingEnabled = true;
+            this.lbFamille.Location = new System.Drawing.Point(12, 12);
+            this.lbFamille.Name = "lbFamille";
+            this.lbFamille.Size = new System.Drawing.Size(388, 212);
+            this.lbFamille.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.PowderBlue;
+            this.textBox1.Location = new System.Drawing.Point(155, 245);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 252);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(112, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Ajout du médicament :";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.PowderBlue;
+            this.button1.Location = new System.Drawing.Point(155, 327);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 53);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Ajouter";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // AjoutMedic
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.Snow;
+            this.ClientSize = new System.Drawing.Size(414, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lbFamille);
+            this.Name = "AjoutMedic";
             this.Text = "AjoutMedic";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox lbFamille;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
     }
 }
